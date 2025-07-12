90c77acc6892858e042856d9609a682240772f32
universe-sm <saniamahajan909@gmail.com>
2025-07-12 23:59:52 +0530

    Update submitted.dart

--- submitted.dart
+++ submitted.dart
@@ -1,8 +1,8 @@
-
 import 'package:flutter/material.dart';
 import 'dart:convert';
 import 'package:http/http.dart' as http;
 import 'package:logger/logger.dart';
+
 final logger = Logger();
 
 class SubmittedDataPage extends StatefulWidget {
@@ -22,17 +22,25 @@ class SubmittedDataPageState extends State<SubmittedDataPage> {
   }
 
   Future<void> fetchSubmittedData() async {
-    final url = Uri.parse('https://api.allorigins.win/raw?url=https://elcapp.42web.io/get_data.php'); // Replace with your actual IP and backend folder path
+    final url = Uri.parse('http://localhost/bloodapp/get_data.php');
 
     try {
       final response = await http.get(url);
 
       if (response.statusCode == 200) {
-        final List<dynamic> data = json.decode(response.body);
-        setState(() {
-          submittedData = data;
-          isLoading = false;
-        });
+        final decoded = json.decode(response.body);
+
+        if (decoded is Map && decoded['data'] is List) {
+          setState(() {
+            submittedData = decoded['data'];
+            isLoading = false;
+          });
+        } else {
+          logger.e("Unexpected response format: $decoded");
+          setState(() {
+            isLoading = false;
+          });
+        }
       } else {
         logger.e("Failed to fetch data");
         setState(() {
@@ -63,43 +71,57 @@ class SubmittedDataPageState extends State<SubmittedDataPage> {
             ),
             Container(
               width: 320,
+              height: 480,
               padding: EdgeInsets.all(10),
               decoration: BoxDecoration(
-               color: Colors.white.withAlpha(13),
+                color: Colors.white.withAlpha(13),
                 borderRadius: BorderRadius.circular(10),
                 border: Border.all(color: Colors.black, width: 2),
               ),
               child: isLoading
                   ? Center(child: CircularProgressIndicator())
                   : submittedData.isEmpty
-                      ? Center(child: Text("No submissions found!"))
-                      : ListView.builder(
-                          shrinkWrap: true,
-                          itemCount: submittedData.length,
-                          itemBuilder: (context, index) {
-                            final item = submittedData[index];
-                            return Column(
-                              crossAxisAlignment: CrossAxisAlignment.start,
-                              children: [
-                                Text(
-                                  'Name: ${item['name']}',
-                                  style: TextStyle(
-                                      fontWeight: FontWeight.bold,
-                                      color: Colors.black),
-                                ),
-                                Text(
-                                  'Email: ${item['email']}',
-                                  style: TextStyle(color: Colors.black),
-                                ),
-                                Text(
-                                  'Age: ${item['age']}',
-                                  style: TextStyle(color: Colors.black),
-                                ),
-                                Divider(color: Colors.black),
-                              ],
-                            );
-                          },
-                        ),
+                  ? Center(child: Text("No submissions found!"))
+                  : ListView.builder(
+                      shrinkWrap: true,
+                      itemCount: submittedData.length,
+                      itemBuilder: (context, index) {
+                        final item = submittedData[index];
+                        return Column(
+                          crossAxisAlignment: CrossAxisAlignment.start,
+                          children: [
+                            Text(
+                              'Name: ${item['name']}',
+                              style: TextStyle(
+                                fontWeight: FontWeight.bold,
+                                color: Colors.black,
+                              ),
+                            ),
+                            Text(
+                              'Email: ${item['email']}',
+                              style: TextStyle(color: Colors.black),
+                            ),
+                            Text(
+                              'Phone: ${item['phone']}',
+                              style: TextStyle(color: Colors.black),
+                            ),
+                            Text(
+                              'DOB: ${item['dob']}',
+                              style: TextStyle(color: Colors.black),
+                            ),
+                            Text(
+                              'Gender: ${item['gender']}',
+                              style: TextStyle(color: Colors.black),
+                            ),
+                            Text(
+                              'Blood Group: ${item['blood_group']}',
+                              style: TextStyle(color: Colors.black),
+                            ),
+                            Divider(color: Colors.black),
+                          ],
+                        );
+                      },
+                    ),
             ),
           ],
         ),
